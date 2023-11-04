f4e9fc8bdfe928eb91175556284d5dd9db49c46a
Marti Serra <martiserramolina99@gmail.com>
2023-11-04 11:44:31 +0100

    Changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require("path");
 
-module.exports = {
+const nodeConfig = {
   entry: "./src/mongu.js",
+  target: "node",
   output: {
-    filename: "mongu.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "mongu.js",
+    library: {
+      type: "commonjs2",
+    },
   },
 };
+
+const webConfig = {
+  entry: "./src/mongu.js",
+  target: "web",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "mongu.js",
+    library: {
+      type: "commonjs2",
+    },
+  },
+};
+
+module.exports = [nodeConfig, webConfig];
