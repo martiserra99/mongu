9c931265b0dfdb45ed23acecee94079f8b1aa074
Marti Serra <martiserramolina99@gmail.com>
2023-11-04 11:18:44 +0100

    Updated configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,8 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/mongu.js",
-  experiments: {
-    outputModule: true,
-  },
   output: {
     filename: "mongu.js",
     path: path.resolve(__dirname, "dist"),
-    library: { type: "module" },
   },
 };
