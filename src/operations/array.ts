861160e5fcbd416f9b13e2a3392df1b8e41c8ada
Marti Serra <martiserramolina99@gmail.com>
2023-11-26 14:09:01 +0100

    Added missing comments

--- src/operations/array.ts
+++ src/operations/array.ts
@@ -3,6 +3,12 @@ import { mongu, Operations, Value, Object } from '../index';
 import { assertArray, assertNumber, assertString } from '../asserts';
 
 export const array: Operations = {
+  /**
+   * Returns the element at the specified array index.
+   * @param args The array and the index.
+   * @param data The variables.
+   * @returns The element at the specified array index.
+   */
   $arrayElemAt(args: [Value, Value], data: Object<Value>): Value {
     const array = mongu(args[0], data);
     assertArray(array);
@@ -10,6 +16,12 @@ export const array: Operations = {
     assertNumber(index);
     return array[index] ?? null;
   },
+  /**
+   * Concatenates arrays to return the concatenated array.
+   * @param args The arrays.
+   * @param data The variables.
+   * @returns The concatenated array.
+   */
   $concatArrays(args: Value[], data: Object<Value>): Value {
     return args.reduce((acc: Value[], expr: Value) => {
       const array = mongu(expr, data);
@@ -17,6 +29,12 @@ export const array: Operations = {
       return acc.concat(array);
     }, []);
   },
+  /**
+   * Selects a subset of an array to return based on the specified condition.
+   * @param args The array, the condition, and the variable name.
+   * @param data The variables.
+   * @returns The subset of the array.
+   */
   $filter(
     args: { input: Value; cond: Value; as: Value },
     data: Object<Value>
@@ -29,6 +47,12 @@ export const array: Operations = {
       return mongu(args.cond, { ...data, [`$${as}`]: value });
     });
   },
+  /**
+   * Returns a specified number of elements from the beginning of an array.
+   * @param args The array and the number of elements.
+   * @param data The variables.
+   * @returns The specified number of elements from the beginning of the array.
+   */
   $firstN(args: { input: Value; n: Value }, data: Object<Value>): Value {
     const array = mongu(args.input, data);
     assertArray(array);
@@ -36,18 +60,36 @@ export const array: Operations = {
     assertNumber(n);
     return array.slice(0, n);
   },
+  /**
+   * Returns a boolean indicating whether a specified value is in an array.
+   * @param args The value and the array.
+   * @param data The variables.
+   * @returns A boolean indicating whether a specified value is in an array.
+   */
   $in(args: [Value, Value], data: Object<Value>): Value {
     const value = mongu(args[0], data);
     const array = mongu(args[1], data);
     assertArray(array);
     return array.includes(value);
   },
+  /**
+   * Searches an array for an occurrence of a specified value and returns the array index of the first occurrence.
+   * @param args The value and the array.
+   * @param data The variables.
+   * @returns The array index of the first occurrence of the specified value.
+   */
   $indexOfArray(args: [Value, Value], data: Object<Value>): Value {
     const array = mongu(args[0], data);
     assertArray(array);
     const value = mongu(args[1], data);
     return array.indexOf(value);
   },
+  /**
+   * Returns a specified number of elements from the end of an array.
+   * @param args The array and the number of elements.
+   * @param data The variables.
+   * @returns The specified number of elements from the end of the array.
+   */
   $lastN(args: { input: Value; n: Value }, data: Object<Value>): Value {
     const array = mongu(args.input, data);
     assertArray(array);
@@ -55,6 +97,12 @@ export const array: Operations = {
     assertNumber(n);
     return array.slice(-n);
   },
+  /**
+   * Applies an expression to each item in an array and returns an array with the applied results.
+   * @param args The array, the variable name, and the expression.
+   * @param data The variables.
+   * @returns The array with the applied results.
+   */
   $map(
     args: { input: Value; as: Value; in: Value },
     data: Object<Value>
@@ -67,6 +115,12 @@ export const array: Operations = {
       return mongu(args.in, { ...data, [`$${as}`]: value });
     });
   },
+  /**
+   * Returns the n largest values in an array.
+   * @param args The array and the number of values.
+   * @param data The variables.
+   * @returns The n largest values in the array.
+   */
   $maxN(args: { input: Value; n: Value }, data: Object<Value>): Value {
     const array = mongu(args.input, data);
     assertArray(array);
@@ -80,6 +134,12 @@ export const array: Operations = {
       })
       .slice(0, n);
   },
+  /**
+   * Returns the n smallest values in an array.
+   * @param args The array and the number of values.
+   * @param data The variables.
+   * @returns The n smallest values in the array.
+   */
   $minN(args: { input: Value; n: Value }, data: Object<Value>): Value {
     const array = mongu(args.input, data);
     assertArray(array);
@@ -93,6 +153,12 @@ export const array: Operations = {
       })
       .slice(0, n);
   },
+  /**
+   * Applies an expression to each element in an array and combines them into a single value.
+   * @param args The array, the initial value, and the expression.
+   * @param data The variables.
+   * @returns The combined value.
+   */
   $reduce(
     args: { input: Value; initialValue: Value; in: Value },
     data: Object<Value>
@@ -104,16 +170,34 @@ export const array: Operations = {
       return mongu(args.in, { ...data, $value: acc, $this: value });
     }, initialValue);
   },
+  /**
+   * Accepts an array expression as an argument and returns an array with the elements in reverse order.
+   * @param args The array.
+   * @param data The variables.
+   * @returns The array with the elements in reverse order.
+   */
   $reverseArray(args: Value, data: Object<Value>): Value {
     const array = mongu(args, data);
     assertArray(array);
     return array.reverse();
   },
+  /**
+   * Counts and returns the total number of items in an array.
+   * @param args The array.
+   * @param data The variables.
+   * @returns The total number of items in the array.
+   */
   $size(args: Value, data: Object<Value>): Value {
     const array = mongu(args, data);
     assertArray(array);
     return array.length;
   },
+  /**
+   * Returns a subset of an array.
+   * @param args The array, the position, and the number of elements.
+   * @param data The variables.
+   * @returns The subset of the array.
+   */
   $slice(args: [Value, Value, Value], data: Object<Value>): Value {
     const array = mongu(args[0], data);
     assertArray(array);
@@ -123,6 +207,12 @@ export const array: Operations = {
     assertNumber(n);
     return array.slice(position, position + n);
   },
+  /**
+   * Sorts an array based on its elements. The sort order is user specified.
+   * @param args The array and the sort order.
+   * @param data The variables.
+   * @returns The sorted array.
+   */
   $sortArray(
     args: { input: Value; sortBy: Value },
     data: Object<Value>
--- src/operations/variable.ts
+++ src/operations/variable.ts
@@ -2,10 +2,10 @@ import { mongu, Operations, Value, Object } from '../index';
 
 export const variable: Operations = {
   /**
-   * Concatenates strings and returns the concatenated string.
-   * @param args An array of strings.
+   * Binds variables for use in the specified expression, and returns the result of the expression.
+   * @param args The variables and the expression.
    * @param data The variables.
-   * @returns The concatenated string.
+   * @returns The result of the expression.
    */
   $let(args: { vars: Object<Value>; in: Value }, data: Object<Value>): Value {
     const vars = Object.fromEntries(
